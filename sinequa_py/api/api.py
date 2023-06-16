ba607a5f0c094e0f3ec55d1bc161786aab7dd187
anisbhsl <bhusal.anish12@gmail.com>
2023-06-16 12:19:56 -0500

    added request module

--- sinequa_py/api/api.py
+++ sinequa_py/api/api.py
@@ -1,11 +1,33 @@
+import requests
+import os 
 class API:
-    def __init__(self, access_token) -> None:
-        self.access_token=access_token
-        pass
+    base_url=""
+    def __init__(self, config) -> None:
+        self.access_token=config["access_token"]
+        self.base_url= config["base_url"]
     
-    def get(self):
-        pass 
+    def _get_headers(self) -> dict:
+        headers={
+            "Authorization": f"Bearer {self.access_token}"
+        }
+        return headers
+    
+    def _get_url(self, endpoint) -> str:
+        return os.path.join(self.base_url, endpoint)
 
-    def post(self):
-        pass 
+    def get(self, endpoint) -> dict:
+        """
+            This method handles GET method.
+        """
+        session= requests.Session()
+        resp=session.get(self._get_url(endpoint=endpoint), headers=self._get_headers)
+        return resp.json()
+
+    def post(self, endpoint, payload) -> dict:
+        """
+            This method handles POST method.
+        """
+        session=requests.Session()
+        resp=session.post(self._get_url(endpoint=endpoint), headers=self._get_headers, json=payload)
+        return resp.json()
 
--- tests/test_search_dataset.py
+++ tests/test_search_dataset.py
@@ -2,5 +2,11 @@ from sinequa_py import Sinequa
 import pytest 
 
 def test_search_dataset():
-    sinequa=Sinequa("xxyyyzzz.Aeworiuwnasdrewr.asleruoiweruqnc")
+    config={
+        "base_url":"http://ec2-100-26-187-210.compute-1.amazonaws.com/api/v1",
+        "access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTaW5lcXVhIiwiaWF0IjoxNjgyNDYxNDI2LCJzaWQiOiJDMkY4QzhGQ0U3Mjk0MjEyOTJDOUM0NEQ4NDM2ODQ3MyIsImtpbmQiOiJhY2Nlc3MiLCJleHAiOjE2OTAyMzc0MjYsInN1YiI6IlNpbmVxdWF8and0LXVzZXIifQ.267q0E-h9jX3TnjUUXbn9ZybkSzv0kssx_mhCw_H55c",
+        "app_name":"vanilla-search",
+        "query_name":"query"
+    }
+    sinequa=Sinequa(config=config)
     sinequa.search_dataset()
