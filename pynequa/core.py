a107b760776febb0e75815f6ecf48d4114e80422
Anish Bhusal <bhusal.anish12@gmail.com>
2023-10-24 01:21:48 -0500

    Release: v0.2.0 (#23)
    * Add documentation for pynequa  (#7)
    
    * added readme and license
    
    * added readthedocs config
    
    * added conf.py
    
    update conf
    
    update conf
    
    removed theme
    
    reset
    
    * fix sphinx docs
    
    * fix typo
    
    * added in gitignore
    
    * Feature: Add github action workflows and tests (#8)
    
    * added build check action
    
    * minor changes
    
    * changes in markdowns
    
    * build on push only
    
    * added mock and actual api unit tests
    
    * update install command for pypi
    
    * add release version
    
    * added usage example and minor fixes (#10)
    
    * added engine.sql method (#13)
    
    * Add classmethod for initializing Sinequa class (#18)
    
    * added classmethod for initializing Sinequa class
    
    * updated docs
    
    * don't send empty params in payload
    
    * don't send empty params in payload (#20)
    
    * fix error with payload generation
    
    * Feature: Update datamodels  (#21)
    
    * updated datamodels to use dataclass
    
    * resolved comments
    
    * updated metadata in docs

--- pynequa/core.py
+++ pynequa/core.py
@@ -1,5 +1,7 @@
-from pynequa.api import API
+from __future__ import annotations
 from typing import Optional, List, Dict
+
+from pynequa.api import API
 from pynequa.models import QueryParams, TreeParams
 
 
@@ -8,17 +10,47 @@ class Sinequa(API):
         Sinequa API Client for Python
 
         Attributes:
+            access_token(str): authentication token for Sinequa
+            base_url(str): base URL for hosted Sinequa instance
             app_name(str): name of Sinequa app
-            query_name(str): name of search query web service 
+            query_name(str): name of search query web service
     '''
-    app_name: str
-    query_name: str
 
-    def __init__(self, config: Dict) -> None:
-        super().__init__(config)
-        self.app_name = config["app_name"]  # name of application
+    def __init__(
+            self,
+            access_token: str,
+            base_url: str,
+            app_name: str = "vanilla-search",
+            query_name: str = "query",
+    ) -> None:
+        super().__init__(access_token=access_token, base_url=base_url)
+        self.app_name = app_name  # name of application
         # name of search query web service
-        self.query_name = config["query_name"]
+        self.query_name = query_name
+
+    @classmethod
+    def from_config(cls, cfg: Dict) -> Sinequa:
+        '''
+        This method creates an instance of Sinequa class from a configuration
+        dictionary.
+
+        Args:
+            cfg (Dict): A dictionary containing configuration parameters.
+                It should include the following keys:
+                - "access_token": A valid access token for authentication.
+                - "base_url": The base URL for the Sinequa API.
+                - "app_name": Name of the Sinequa application.
+                - "query_name": Name of the search query service.
+
+        Returns:
+            Sinequa: An instance of Sinequa class initialized with given configuration
+        '''
+        return cls(
+            access_token=cfg["access_token"],
+            base_url=cfg["base_url"],
+            app_name=cfg["app_name"],
+            query_name=cfg["query_name"],
+        )
 
     @staticmethod
     def _prepare_kwargs(payload: Dict, kwargs: Dict) -> Dict:
@@ -28,10 +60,10 @@ class Sinequa(API):
 
     def search_app(self, pre_login: bool = False, mode: str = "debug") -> Dict:
         '''
-            This method retrieves SBA configuration before and after login. 
+            This method retrieves SBA configuration before and after login.
 
-            Args: 
-                pre_login(bool): false by default. 
+            Args:
+                pre_login(bool): false by default.
                 mode(str): debug by default (debug|release)
         '''
         endpoint = "search.app"
@@ -46,13 +78,13 @@ class Sinequa(API):
                        datasets: Optional[List[str]]) -> Dict:
         '''
         This method retrieves datasets through SQL queries. The response is a
-        list of available datasets with their respective names and descriptions. 
+        list of available datasets with their respective names and descriptions.
 
         Args:
             parameters(dict): dictionary of parameters that can be used in SQL fragments
             datasets(list): list of datasets
         Returns:
-            Dict: search dataset response  
+            Dict: search dataset response
 
         '''
         endpoint = "search.dataset"
@@ -70,16 +102,16 @@ class Sinequa(API):
         This method performs search query.
 
         Args:
-            query_params(QueryParams): query parameters as defined in QueryParams class 
+            query_params(QueryParams): query parameters as defined in QueryParams class
 
         Returns:
-            Dict: response data of this request 
+            Dict: response data of this request
         '''
         endpoint = "search.query"
 
         payload = {
             "app": self.app_name,
-            "query": query_params._prepare_query_args(
+            "query": query_params.generate_payload(
                 query_name=self.query_name)
         }
 
@@ -87,13 +119,13 @@ class Sinequa(API):
 
     def query_intent(self, intent_text: str) -> Dict:
         '''
-        This method evaluates SBA query intents from query intent sets. 
+        This method evaluates SBA query intents from query intent sets.
 
         Args:
             intent_text(str): indicates the text that should trigger query intent
 
         Returns:
-            Dict: Query Intent response 
+            Dict: Query Intent response
 
         '''
         endpoint = "queryintent"
@@ -113,14 +145,14 @@ class Sinequa(API):
     def search_profile(self, profile_name: str, query_params: QueryParams,
                        response_type: str = "SearchCursor") -> Dict:
         '''
-        This method searches for Sienequa profile. 
+        This method searches for Sienequa profile.
 
         Args:
             profile_name(str): profile name
-            response_type(str): default will be SearchCursor 
+            response_type(str): default will be SearchCursor
             query_params(QueryParams): will carry  query parameters in payload
 
-        Returns: 
+        Returns:
             Dict: response data for Sinequa profile search.
         '''
         endpoint = "search.profile"
@@ -128,7 +160,7 @@ class Sinequa(API):
         payload = {
             "profile": profile_name,
             "responsetype": response_type,
-            "query": query_params._prepare_query_args(query_name=self.query_name),
+            "query": query_params.generate_payload(query_name=self.query_name),
         }
 
         return self.post(endpoint=endpoint, payload=payload)
@@ -136,16 +168,16 @@ class Sinequa(API):
     def search_user_settings(self, action: str = "load",
                              user_settings: Dict = {}) -> Dict:
         '''
-        This method provides user settings 
+        This method provides user settings
 
         Args:
             app_name(str): name of application for which user setting should be handled
             action(str): search action (load|save|patch)
-            user_settings(Dict): user settings to be saved or patched (see official 
+            user_settings(Dict): user settings to be saved or patched (see official
             documentation for more info)
 
-        Returns: 
-            Dict: search response based upon action 
+        Returns:
+            Dict: search response based upon action
         '''
         endpoint = "search.usersettings"
         payload = {
@@ -159,7 +191,7 @@ class Sinequa(API):
     def search_preview(self,   query_params: QueryParams, action: str = "get",
                        origin: str = "", id: str = "") -> Dict:
         '''
-        This method retrieves preview data for a product. 
+        This method retrieves preview data for a product.
 
         Args:
             query_params(QueryParams): query params with text to be searched
@@ -177,7 +209,7 @@ class Sinequa(API):
             "action": action,
             "id": id,
             "origin": origin,
-            "query": query_params._prepare_query_args(
+            "query": query_params.generate_payload(
                 query_name=self.query_name)
         }
 
@@ -196,7 +228,7 @@ class Sinequa(API):
             max_count(int): maximum of number of documents to include in export(optiona)
 
         Returns:
-            Dict: response for search export 
+            Dict: response for search export
         '''
         endpoint = "search.queryexport"
         payload = {
@@ -239,17 +271,17 @@ class Sinequa(API):
 
         Args:
             source_doc_id(str): identifier of document for which to retrieve similar documents
-            query_params(QueryParams): query params 
+            query_params(QueryParams): query params
 
-        Returns: 
-            Dict: search response for similar documents 
+        Returns:
+            Dict: search response for similar documents
         '''
         endpoint = "search.similardocuments"
 
         payload = {
             "app": self.app_name,
             "sourceDocumentId": source_doc_id,
-            "query": query_params._prepare_query_args(
+            "query": query_params.generate_payload(
                 query_name=self.query_name)
         }
 
@@ -257,18 +289,18 @@ class Sinequa(API):
 
     def search_query_links(self, web_sevice: str, query_params: QueryParams) -> Dict:
         '''
-        This method retrieves sponsored links for the passed query. 
+        This method retrieves sponsored links for the passed query.
 
         Args:
             web_service(str): name of corresponding sponsored links web service
-        Returns: 
-            Dict: response for query links search 
+        Returns:
+            Dict: response for query links search
 
         '''
         endpoint = "search.querylinks"
         payload = {
             "webService": web_sevice,
-            "query": query_params._prepare_query_args(
+            "query": query_params.generate_payload(
                 query_name=self.query_name)
         }
         return self.post(endpoint=endpoint, payload=payload)
@@ -277,20 +309,20 @@ class Sinequa(API):
                        avg_column: str, ratings_distribution: List[str], rating: int,
                        update_doc_weight: bool) -> Dict:
         '''
-        This method makes it possible to get, set and delete ratings associated with a 
-        document. 
+        This method makes it possible to get, set and delete ratings associated with a
+        document.
 
         Args:
             action(str): get|set|delete
-            docid(str): document id 
+            docid(str): document id
             ratings_column(str): name of column in which to store rating
             avg_column(str): name of column to store average rating
             ratings_distribution(List[str]): array of possible ratings
             rating(int): sets the action parameter (optional)
-            update_doc_weight(bool): indicates whether to update the doc weight 
+            update_doc_weight(bool): indicates whether to update the doc weight
                                     according to rating (optional)
-        Returns: 
-            Dict: response for ratings search 
+        Returns:
+            Dict: response for ratings search
         '''
         endpoint = "search.ratings"
         payload = {
@@ -310,22 +342,22 @@ class Sinequa(API):
     def search_profile_subtree(self, profile: str, query_params: QueryParams,
                                tree_params: TreeParams) -> Dict:
         '''
-        This method allows to select a subtree. 
+        This method allows to select a subtree.
 
-        Args: 
+        Args:
             profile(str): profile name
-            query_params(QueryParams): search parameters 
+            query_params(QueryParams): search parameters
             tree_params(TreeParams): defines the sub-tree to retrieve
 
-        Returns: 
-            Dict: returns subtree profile response 
+        Returns:
+            Dict: returns subtree profile response
         '''
         endpoint = "search.profile.subtree"
         payload = {
             "profile": profile,
-            "query": query_params._prepare_query_args(
+            "query": query_params.generate_payload(
                 query_name=self.query_name),
-            "tree": tree_params._generate_tree_params_payload()
+            "tree": tree_params.generate_payload()
         }
         return self.post(endpoint=endpoint, payload=payload)
 
@@ -371,8 +403,23 @@ class Sinequa(API):
         endpoint = "suggestField"
         pass
 
-    def engine_sql(self):
+    def engine_sql(self, sql: str, max_rows: int = 1000) -> Dict:
         '''
+        This method runs classic SQL query on the database engine.
+
+        Args:
+            sql (str): Query to send
+            max_rows (int): Maximum number of rows a query can return (default: 1000)
+        Returns:
+            Dict: response for sql query
         '''
         endpoint = "engine.sql"
-        pass
+
+        payload = {
+            "sql": sql,
+            "maxRows": 1000,
+            "pretty": True,
+            "output": "json"
+        }
+
+        return self.post(endpoint=endpoint, payload=payload)
