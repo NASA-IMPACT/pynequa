4fabf4744606997dbc5c07c06310c7268a10106c
Nish <nishanpantha@gmail.com>
2024-02-27 16:06:02 -0600

    Bugfix advanced params (#28)
    * Remove operator and value in payload if they are None in AdvancedParams
    
    Sample:
    
    ```python
    {
        'app': 'vanilla-search',
        'query': {
            'name': 'query',
            'text': 'himawari',
            'isFirstpage': False,
            'strictRefine': False,
            'removeDuplicates': False,
            'action': 'search',
            'page': 1,
            'pageSize': 10,
            'advanced': {
                'collection': '/user_needs_database/snwg-assessments-2020/',
                # 'value': None, 'operator': None,
            }
        }
    }
    ```
    
    * Bugfix payload generation for AdvancedParams
    
    Now col_name and values are checked to see if empty.
    
    * added support for advanced params according to documentation
    
    ---------
    
    Co-authored-by: anisbhsl <bhusal.anish12@gmail.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
-from pynequa.models import QueryParams
+from pynequa.models import QueryParams, AdvancedParams
 import unittest
 import logging
+import json
 
 
 class TestQueryParams(unittest.TestCase):
@@ -12,10 +13,12 @@ class TestQueryParams(unittest.TestCase):
         """
         qp = QueryParams(
             name="query",
-            search_text="What was Landsat-9 launched?"
+            search_text="What was Landsat-9 launched?",
+            page_size=20,
         )
 
         payload = qp.generate_payload()
+        print(payload)
         logging.debug(payload)
 
         keys_which_must_be_in_payload = [
@@ -29,6 +32,58 @@ class TestQueryParams(unittest.TestCase):
         for key in keys_which_must_be_in_payload:
             if key not in payload:
                 self.assertEqual(key, "test", f"{key} is mising in payload")
+
+    def test_query_params_with_advanced_params(self):
+        """
+            Test if advanced params are correctly
+            generated in query param payload or not.
+        """
+
+        ap1 = AdvancedParams(
+            col_name="collection",
+            col_value="accounting"
+        )
+
+        ap2 = AdvancedParams(
+            col_name="docformat",
+            col_value=["pdf", "docx"]
+        )
+
+        ap3 = AdvancedParams(
+            col_name="modified",
+            value="2019-01-01",
+            operator=">="
+        )
+
+        ap4 = AdvancedParams(
+            col_name="modified",
+            value="2019-12-31",
+            operator="<="
+        )
+
+        qp = QueryParams(
+            name="query",
+            search_text="What was Landsat-9 launched?",
+            advanced=[
+                ap1,
+                ap2,
+                ap3,
+                ap4
+            ]
+        )
+
+        payload = qp.generate_payload()
+
+        expected_payload = {
+            "collection": "accounting",
+            "docformat": ["pdf", "docx"],
+            "modified": [
+                {"value": "2019-01-01", "operator": ">="},
+                {"value": "2019-12-31", "operator": "<="}
+            ]
+        }
+
+        assert payload["advanced"] == expected_payload
 
 
 if __name__ == '__main__':
